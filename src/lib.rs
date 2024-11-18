378a89f7ce3b1c159301927fd4b4832d642a6d22
Rishabh <rish.aryal@gmail.com>
2024-11-18 16:15:33 +0545

    added config support, request validator, and basic response

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+mod core;
+
+pub use crate::core::config;
+pub use crate::core::http_validator;
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
+use std::io::Write;
+use std::path::Path;
+use std::process::exit;
 
+use servw::config::Config;
+use servw::http_validator::HttpValidator;
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:3000")?;
+
+    let mut config = Config::new();
+    
+    match config.parse("http.conf") {
+        Ok(_) => {
+            println!("Config parsed successfully");
+        },
+        Err(e) => {
+            println!("Config parsing error: {}", e);
+            exit(1);
+        }
+    }
+
+    // check if the root folder exists
+    if !Path::new(&config.root()).exists() {
+        println!("Error: Root folder does not exist");
+        exit(1);
+    }
+
+    let port = config.listen();
+    let listener = TcpListener::bind("127.0.0.1:".to_string() + port)?;
 
     for stream in listener.incoming() {
-
         // handle in a thread so that we can keep listening for more connections
-        println!("Incoming connection");
         std::thread::spawn(move || {
             if let Err(e) = handle_connection(stream.unwrap()) {
                 println!("Connection error: {}", e);
@@ -17,16 +39,18 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
-
 }
+
 fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> { 
-    println!("Connection established");
-    let mut buffer = [0;1024];
-    stream.read(&mut buffer)?;
-    let _data = String::from_utf8_lossy(&buffer);
+    // validate if the request is a valid HTTP request
+    let mut http_validator = HttpValidator::new(&mut stream);
+    if !http_validator.validate() {
+        stream.write(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n Invalid HTTP request deteced!")?;
+        return Ok(());
+    }
 
     // send a response back to the client
-    stream.write(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n")?;
+    stream.write(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n Welcome to ServW")?;
     println!("Connection terminated");
 
     Ok(())
